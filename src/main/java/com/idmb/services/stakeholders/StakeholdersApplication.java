b209c8b8791a9f7014251940b6c30a8616261ccd
aadit <aadit.pant@8squarei.com>
2024-10-05 20:25:29 +0545

    first commit

---
+++ src/main/java/com/idmb/services/stakeholders/StakeholdersApplication.java
@@ -0,0 +1,13 @@
+package com.idmb.services.stakeholders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakeholdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakeholdersApplication.class, args);
+	}
+
+}
